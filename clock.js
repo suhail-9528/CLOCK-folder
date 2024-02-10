875e3e028d7bc74776578272445c231219a3e4a1
Your Name <you@example.com>
2024-02-10 09:12:20 +0530

    upload to clock folder

---
+++ clock.js
@@ -0,0 +1,38 @@
+
+
+  let hr= document.getElementById('hours') ;
+  let min= document.getElementById('min');
+  let sec= document.getElementById('sec');
+  
+  function displayTime(){
+
+    let date = new Date(); 
+
+   //getinh hours, mins, secs from date
+     let hh= date.getHours();
+     let mm = date.getMinutes();
+     let ss = date.getSeconds();
+
+     
+     let hRotation = 30*hh + mm/2;
+     let mRotation = 6*mm;
+     let sRotation = 6*ss;
+
+     hr.style.transform= `rotate(${hRotation}deg)`; 
+     min.style.transform= `rotate(${mRotation}deg)`; 
+     sec.style.transform= `rotate(${sRotation}deg)`;
+    
+  } 
+  setInterval(displayTime, 1000);
+
+
+
+
+
+
+
+
+
+
+
+  
